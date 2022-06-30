263ae19db224020be3119b808ed54478c826d23b
xinghe <1329749225@qq.com>
2022-06-30 15:02:57 +0800

    第一次提交：初始化用户中心项目

---
+++ src/test/java/com/dyl/usercenter/UsercenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyl.usercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsercenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
